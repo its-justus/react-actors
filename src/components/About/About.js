5f3825fe33f34cf98f0a591ffcb9b2113af837cc
its-justus <ianjohnson9042@gmail.com>
2020-06-09 12:07:23 -0500

    all components working correctly

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h2>About</h2>
+                <p>The React Actors is a troupe of improvisational actors that performs live shows across the globe!</p>
+            </div>
+        );
+    }
+}
+
+export default About;
